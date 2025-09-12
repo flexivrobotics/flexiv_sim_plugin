0098a3f8c73394e707a4e5afefb425b5d33da924
Peizhang Zhu <peizhang.zhu@flexiv.com>
2025-09-11 21:56:23 -0700

    Upgrade Fast-DDS to 2.6.10 and its dependencies accordingly

--- thirdparty/scripts/install_Fast-DDS.sh
+++ thirdparty/scripts/install_Fast-DDS.sh
@@ -3,10 +3,6 @@
 set -e
 echo "Installing Fast-DDS"
 
-# Get install directory and number of parallel build jobs as script arguments
-INSTALL_DIR=$1
-NUM_JOBS=$2
-
 # Clone source code
 if [ ! -d Fast-DDS ] ; then
   git clone https://github.com/eProsima/Fast-DDS.git
@@ -17,16 +13,12 @@ fi
 
 # Use specific version
 git fetch -p
-git checkout v2.6.7
+git checkout v2.6.10
 git submodule update --init --recursive
 
 # Configure CMake
 mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release \
-         -DBUILD_SHARED_LIBS=OFF \
-         -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-         -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-         -DCMAKE_PREFIX_PATH=$INSTALL_DIR \
+cmake .. $SHARED_CMAKE_ARGS \
          -DTHIRDPARTY_Asio=ON \
          -DCOMPILE_EXAMPLES=OFF \
          -DSQLITE3_SUPPORT=OFF \
