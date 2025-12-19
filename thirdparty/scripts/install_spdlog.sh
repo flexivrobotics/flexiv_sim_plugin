f4e1fd84bb9a4a2c199576133ae51f2c798a9a1e
Peizhang Zhu <peizhang.zhu@flexiv.com>
2025-12-19 12:55:06 -0800

    Update version of spdlog to be the same as the apt installed version on Ubuntu 22.04

--- thirdparty/scripts/install_spdlog.sh
+++ thirdparty/scripts/install_spdlog.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Use a specific version
-VER_TAG=v1.14.1
+# Same as the apt installed version on Ubuntu 22.04
+VER_TAG=v1.9.2
 
 # Clone source code
 if [ ! -d spdlog ] ; then
